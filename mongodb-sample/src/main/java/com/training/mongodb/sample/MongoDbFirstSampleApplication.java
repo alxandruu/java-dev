94bca5b0c4ed2bff4f1703337bca6ef706a0280e
Alexandru Botas <43759266+alxandruu@users.noreply.github.com>
2023-08-17 20:00:46 +0200

    feat(mongodb-sample)

---
+++ mongodb-sample/src/main/java/com/training/mongodb/sample/MongoDbFirstSampleApplication.java
@@ -0,0 +1,13 @@
+package com.training.mongodb.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbFirstSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbFirstSampleApplication.class, args);
+	}
+
+}
---
+++ mongodb-sample/src/test/java/com/training/mongodb/sample/MongoDbFirstSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.mongodb.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbFirstSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
